2885afb4275a9d51abb3682e610a06a87dd025c1
Jovie Crammer <znac176@live.rhul.ac.uk>
2026-05-27 14:49:10 +0100

    Initial Spring Boot project setup

---
+++ src/test/java/com/jovie/lineage/LineageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jovie.lineage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LineageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
